7baebc8190d7384d439d3813b66d65bfb4e9beab
Ryan Walker <Ryan.Walker@amtrustgroup.com>
2018-12-03 16:20:45 -0500

    cleaned up day 3 code and commented it out for future reference

--- 03/one.py
+++ 03/one.py
@@ -2,9 +2,11 @@ import numpy as np
 
 
 SQUARE_SIZE = 1000
+COLLISION_SYMBOL = 'X'
+CLAIM_SPACE_SYMBOL = '#'
 
 
-class FabricSquare:
+class FabricRectangle:
     def __init__(self, claim_id, x, y, width, height):
         self.claim_id = claim_id
         self.x = x
@@ -12,6 +14,8 @@ class FabricSquare:
         self.width = width
         self.height = height
 
+        # create a list of all the row/column coordinates the fabric rectangle takes up
+        # so that it can easily be iterated over and mapped to the main fabric piece
         coordinates = []
         for row in range(self.y, self.y + self.height):
             for cell in range(self.x, self.x + self.width):
@@ -21,11 +25,14 @@ class FabricSquare:
 
 
 def parse_input(input_data):
+    """
+    Takes a line for the input and parses the string to a FabricRectangle instance
+    """
     cleaned_input = input_data.replace('@ ', '').replace(':', '')
     split_data = cleaned_input.split(' ')
     xy = split_data[1].split(',')
     wh = split_data[2].split('x')
-    return FabricSquare(
+    return FabricRectangle(
         claim_id=split_data[0],
         x=int(xy[0]),
         y=int(xy[1]),
@@ -35,33 +42,46 @@ def parse_input(input_data):
 
 
 def main(input_data):
+    # create an empty array with the main fabric size, one cell representing a square inch
     main_fabric = np.empty((SQUARE_SIZE, SQUARE_SIZE), dtype=np.str)
-    print(main_fabric)
+    # take the inputed list of strings and use the parse function
+    # and map to get back a list of FabricRectangle instances
     parsed_input = list(map(parse_input, input_data))
 
+    # iterate over all of the FabricRectangle instances
     for fabric_claim in parsed_input:
+        # iterate over all of the coordinates in the FabricRectangle instance
         for coordinate in fabric_claim.coordinates:
             row, cell = coordinate
-            if main_fabric[row][cell] == 'X':
-                continue
-            elif main_fabric[row][cell] == '#':
-                main_fabric[row][cell] = 'X'
-            else:
-                main_fabric[row][cell] = '#'
 
+            # if a space is already claimed or a collision already exists,
+            # mark the space with a collision symbol
+            if main_fabric[row][cell] == COLLISION_SYMBOL \
+                    or main_fabric[row][cell] == CLAIM_SPACE_SYMBOL:
+                main_fabric[row][cell] = COLLISION_SYMBOL
+            # if space is unclaimed, mark it with claim space symbol
+            else:
+                main_fabric[row][cell] = CLAIM_SPACE_SYMBOL
+
+    # flatten the main fabric array so that it can more easily be iterated over
     flat_array = main_fabric.flatten()
 
+    # initialize a variable to count the number of collisions
     total_collisions = 0
 
+    # iterate over the flattened array and count every time a collision occured
+    # (every time a space has the COLLSION_SYMBOL)
     for cell in flat_array:
-        if cell == 'X':
+        if cell == COLLISION_SYMBOL:
             total_collisions += 1
 
+    # return the total collision
     return total_collisions
 
 
 if __name__ == "__main__":
     with open("input.txt") as file:
+        # read the input, split by lines and use it as argument to main function
         input_data = file.read().split('\n')
         output = main(input_data)
         print(output)
--- 03/two.py
+++ 03/two.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 SQUARE_SIZE = 1000
+COLLISION_SYMBOL = 'X'
+CLAIM_SPACE_SYMBOL = '#'
 
 
 class FabricSquare:
@@ -12,6 +14,8 @@ class FabricSquare:
         self.width = width
         self.height = height
 
+        # create a list of all the row/column coordinates the fabric rectangle takes up
+        # so that it can easily be iterated over and mapped to the main fabric piece
         coordinates = []
         for row in range(self.y, self.y + self.height):
             for cell in range(self.x, self.x + self.width):
@@ -21,6 +25,9 @@ class FabricSquare:
 
 
 def parse_input(input_data):
+    """
+    Takes a line for the input and parses the string to a FabricRectangle instance
+    """
     cleaned_input = input_data.replace('@ ', '').replace(':', '')
     split_data = cleaned_input.split(' ')
     xy = split_data[1].split(',')
@@ -35,34 +42,56 @@ def parse_input(input_data):
 
 
 def main(input_data):
+    # create an empty array with the main fabric size, one cell representing a square inch
     main_fabric = np.empty((SQUARE_SIZE, SQUARE_SIZE), dtype=np.str)
+    # take the inputed list of strings and use the parse function
+    # and map to get back a list of FabricRectangle instances
     parsed_input = list(map(parse_input, input_data))
 
-    found_id = []
-
+    # iterate over all of the FabricRectangle instances
     for fabric_claim in parsed_input:
+        # iterate over all of the coordinates in the FabricRectangle instance
         for coordinate in fabric_claim.coordinates:
             row, cell = coordinate
-            if main_fabric[row][cell] == 'X' or main_fabric[row][cell] == '#':
-                main_fabric[row][cell] = 'X'
+            # if a space is already claimed or a collision already exists,
+            # mark the space with a collision symbol
+            if main_fabric[row][cell] == COLLISION_SYMBOL \
+                    or main_fabric[row][cell] == CLAIM_SPACE_SYMBOL:
+                main_fabric[row][cell] = COLLISION_SYMBOL
+            # if space is unclaimed, mark it with claim space symbol
             else:
-                main_fabric[row][cell] = '#'
+                main_fabric[row][cell] = CLAIM_SPACE_SYMBOL
 
+    # initialize a variable to hold ID of the fabric
+    # piece with no collisions once it has been found
+    found_id = None
+    # iterate back over all of the FabricRectangle instances
     for fabric_claim in parsed_input:
+        # initialize a variable declaring whether or not the
+        # current fabricRectangle has any collisions with other pieces
         no_collisions = True
+        # iterate over all of the coordinates for the current fabric piece
         for coordinate in fabric_claim.coordinates:
             row, cell = coordinate
-            if main_fabric[row][cell] == 'X':
+            # if any of the coordinates on the current fabric peice map to
+            # a collision symbol on the main fabric array, change the 'no_collisions'
+            # variable to false to indicate the piece is not the correct piece
+            if main_fabric[row][cell] == COLLISION_SYMBOL:
                 no_collisions = False
 
+        # if this piece of did not encounter any collision symbols
+        # then we have found the piece we are look for. Set found_id
+        # variable this piece's id, break the loop and return the id
         if no_collisions:
-            found_id.append(fabric_claim.claim_id)
+            found_id = fabric_claim.claim_id
+            break
 
     return found_id
 
 
 if __name__ == "__main__":
     with open("input.txt") as file:
+        # read the input, split by lines and use it as argument to main function
         input_data = file.read().split('\n')
         output = main(input_data)
         print(output)
